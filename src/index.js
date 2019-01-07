04336b51dbaaa89bb3e63748ffbe06ea4b4a35ab
Sergio Perez <sperez4mba@gmail.com>
2019-01-07 17:47:19 +0100

    Use port 5000 instead

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ const os = require('os');
 const app = express();
 
 app.use(express.static('public'));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+app.listen(5000, () => console.log('Listening on port 5000!'));
