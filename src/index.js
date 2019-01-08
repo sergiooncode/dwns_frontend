e2638d88966983af5bfdb7c71173bf09d7cf78e0
Sergio Perez <sperez4mba@gmail.com>
2019-01-08 19:01:16 +0100

    Add route to controller

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const os = require('os');
 
 const app = express();
 
-app.use(express.static('public'));
+app.use('/', express.static('public'));
 app.listen(5000, () => console.log('Listening on port 5000!'));
