1e6732c21f0b126fc57d1fb52ecc1cbd9607784b
Sergio Perez <sperez4mba@gmail.com>
2019-01-08 19:13:25 +0100

    Make express app listen on public interface

--- src/index.js
+++ src/index.js
@@ -4,4 +4,4 @@ const os = require('os');
 const app = express();
 
 app.use('/', express.static('public'));
-app.listen(5000, () => console.log('Listening on port 5000!'));
+app.listen(5000, '0.0.0.0', () => console.log('Listening on port 5000!'));
